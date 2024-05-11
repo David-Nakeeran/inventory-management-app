7bc412c8f6e50014f8322ada46d83ccd33c9dd5b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-05-11 13:18:39 +0100

    Uncommented routes for item and category

--- routes/catalog.js
+++ routes/catalog.js
@@ -10,22 +10,22 @@ router.get("/", item_controller.index);
 
 /* Item Routes */
 // GET request for creating a Item. NOTE This must come before routes that display Item (uses id).
-// router.get("/item/create", item_controller.item_create_get);
+router.get("/item/create", item_controller.item_create_get);
 
 // POST request for creating item.
-// router.post("/item/create", item_controller.item_create_post);
+router.post("/item/create", item_controller.item_create_post);
 
 // GET request to delete item.
-// router.get("/item/:id/delete", item_controller.item_delete_get);
+router.get("/item/:id/delete", item_controller.item_delete_get);
 
 // POST request to delete item.
-// router.post("/item/:id/delete", item_controller.item_delete_post);
+router.post("/item/:id/delete", item_controller.item_delete_post);
 
 // GET request to update item.
-// router.get("/item/:id/update", item_controller.item_update_get);
+router.get("/item/:id/update", item_controller.item_update_get);
 
 // POST request to update item.
-// router.post("/item/:id/update", item_controller.item_update_post);
+router.post("/item/:id/update", item_controller.item_update_post);
 
 // GET request for one item.
 router.get("/item/:id", item_controller.item_detail);
@@ -35,22 +35,21 @@ router.get("/items", item_controller.item_list);
 
 /* Category Routes */
 // GET request for creating a Item. NOTE This must come before routes that display Item (uses id).
-// router.get("/category/create", category_controller.category_create_get);
-
+router.get("/category/create", category_controller.category_create_get);
 // POST request for creating category.
-// router.post("/category/create", category_controller.category_create_post);
+router.post("/category/create", category_controller.category_create_post);
 
 // GET request to delete category.
-// router.get("/category/:id/delete", category_controller.category_delete_get);
+router.get("/category/:id/delete", category_controller.category_delete_get);
 
 // POST request to delete category.
-// router.post("/category/:id/delete", category_controller.category_delete_post);
+router.post("/category/:id/delete", category_controller.category_delete_post);
 
 // GET request to update category.
-// router.get("/category/:id/update", category_controller.category_update_get);
+router.get("/category/:id/update", category_controller.category_update_get);
 
 // POST request to update category.
-// router.post("/category/:id/update", category_controller.category_update_post);
+router.post("/category/:id/update", category_controller.category_update_post);
 
 // GET request for one category.
 router.get("/category/:id", category_controller.category_detail);
